9b6df1aa0f48bd2fe9c6150eb9f1f55cb4ccfc81
ChenLei <chenlbupt@163.com>
2017-05-22 00:25:51 +0800

    update properties file

--- src/main/java/com/swad/controller/HelloController.java
+++ src/main/java/com/swad/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.swad.controller;
 
+import com.swad.model.StudentProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private StudentProperties studentProperties;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say(){
-        return "Hello Spring Boot!";
+        return studentProperties.getSize();
     }
 }
---
+++ src/main/java/com/swad/model/StudentProperties.java
@@ -0,0 +1,33 @@
+package com.swad.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by chenlei on 2017/5/22.
+ */
+
+@Component
+@ConfigurationProperties(prefix = "student")
+public class StudentProperties {
+
+    private String size;
+
+    private Integer age;
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
